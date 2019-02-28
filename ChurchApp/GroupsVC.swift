8c0d9588daadf5ace1fa34cc37c761352b27873d
Jessica  Gomes <jessica05og@gmail.com>
2019-02-28 13:58:32 +0000

    1- Home 2- Contact OK 3- Give 4- Calendar 5 - Login OK

---
+++ ChurchApp/GroupsVC.swift
@@ -0,0 +1,30 @@
+//
+//  GroupsVC.swift
+//  ChurchApp
+//
+//  Created by Jessica  Gomes on 2/27/19.
+//  Copyright © 2019 Jessica  Gomes. All rights reserved.
+//
+
+import UIKit
+
+class GroupsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ChurchApp/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChurchApp
+//
+//  Created by Jessica  Gomes on 2/27/19.
+//  Copyright © 2019 Jessica  Gomes. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
