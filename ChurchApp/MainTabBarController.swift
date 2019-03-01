da031ac59aa274e985e3f1e714c4998616d61263
Jessica  Gomes <jessica05og@gmail.com>
2019-03-01 18:56:10 +0000

    Login Working

--- ChurchApp/MainTabBarController.swift
+++ ChurchApp/MainTabBarController.swift
@@ -7,16 +7,16 @@
 //
 
 import UIKit
+
 import Firebase
-import FirebaseUI
+import FirebaseAuth
 
 
 class MainTabBarController: UIViewController {
 
-    //Button user submiting a email
-    @IBOutlet var emailTF: UITextField!
-    //Button password submiting a password
-    @IBOutlet var passwordTF: UITextField!
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,44 +25,105 @@ class MainTabBarController: UIViewController {
         
         }
         
-    @IBAction func loginButton(_ sender: UIButton) {
-        //Get the Default auth UI object
-        let authUI = FUIAuth.defaultAuthUI()
+//    @IBAction func loginButton(_ sender: UIButton) {
+//        //Get the Default auth UI object
+//        let authUI = FUIAuth.defaultAuthUI()
+//
+//        guard authUI != nil else {
+//
+//            //Log the error
+//            return
+//        }
+//
+//        // Set ourselves as the delegate
+//        authUI?.delegate = self
+//        // Get a reference to the auth UI view Controller
+//
+//        let authViewController = authUI!.authViewController()
+//        // show it
+//        present(authViewController, animated: true, completion: nil)
+//
+//    }
+    
+    
+//    @IBAction func SingIn(_ sender: UIButton) {
+//
+//
+//        Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+//
+//            if user != nil {
+//                print("User has sign Up")
+//            }
+//            if error != nil{
+//                print("=(")
+//            }
+//        }
+//
+//    }
+//
+//
+//
+//    @IBAction func SingUp(_ sender: UIButton) {
+//        Auth.auth().createUser(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+//
+//            if user != nil {
+//                print("User has sign up")
+//            }
+//            if error != nil{
+//                print("=(")
+//            }
+//        }
+//
+//    }
+    
+    
+    @IBAction func singIn(_ sender: UIButton) {
         
-        guard authUI != nil else {
-
-            //Log the error
-            return
+        Auth.auth().signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+            
+            if user != nil {
+                print("User has sign Up")
+            }
+            if error != nil{
+                print("=(")
+            }
         }
         
-        // Set ourselves as the delegate
-        authUI?.delegate = self
-        // Get a reference to the auth UI view Controller
-        
-        let authViewController = authUI!.authViewController()
-        // show it
-        present(authViewController, animated: true, completion: nil)
         
     }
-}
     
-extension MainTabBarController: FUIAuthDelegate{
-    
-    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
-        
-        // check if there was an error
-        guard error == nil else {
-            //Log the error
-            return
+    @IBAction func signUp(_ sender: UIButton) {
+        Auth.auth().createUser(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+            
+            if user != nil {
+                print("User has sign up")
+            }
+            if error != nil{
+                print("=(")
+            }
         }
-        
-       // authDataResult?.user.uid
-        
-        performSegue(withIdentifier: "GoHome", sender: self)
-        
     }
     
     
 }
+    
+//extension MainTabBarController: FUIAuthDelegate{
+//
+//    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+//
+//        // check if there was an error
+//        guard error == nil else {
+//            //Log the error
+//            return
+//        }
+//
+//       // authDataResult?.user.uid
+//
+//        performSegue(withIdentifier: "GoHome", sender: self)
+//
+//    }
+//
+//
+//}
 
 
